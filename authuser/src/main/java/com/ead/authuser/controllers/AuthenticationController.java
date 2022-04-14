8905d01ec7fe00096dcf78d3cdc7073c121bf212
Igor Frey <PRICILAOLIVEIRAS21@OUTLOOK.COM.BR>
2022-04-13 22:40:36 -0300

    Usando JSONView para mapear objetos de entrada para serviços de atualização do UserController

--- authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -18,6 +18,7 @@ import com.ead.authuser.enums.UserStatus;
 import com.ead.authuser.enums.UserType;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -29,7 +30,8 @@ public class AuthenticationController {
 
 	// Object pois podemos retornar não só um tipo
 	@PostMapping("/signup")
-	public ResponseEntity<Object> registerUser(@RequestBody UserDto userDto) {
+	public ResponseEntity<Object> registerUser(@RequestBody
+			@JsonView(UserDto.UserView.RegistrationPost.class) UserDto userDto) {
 
 		if (userService.existsByUsername(userDto.getUsername())) {
 			return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: username is already taken!");
