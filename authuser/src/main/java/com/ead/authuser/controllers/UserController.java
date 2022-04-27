5b466a19fa41d5affeb392f1a87d3b22e9796a88
Igor Frey <igorfreyy97@gmail.com>
2022-04-26 22:18:48 -0300

    Criação de filtros com Specification

--- authuser/src/main/java/com/ead/authuser/controllers/UserController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -25,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ead.authuser.dtos.UserDto;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
+import com.ead.authuser.specification.SpecificationTemplate;
 import com.fasterxml.jackson.annotation.JsonView;
 
 @RestController
@@ -36,10 +37,11 @@ public class UserController {
 	UserService userService;
 
 	@GetMapping
-	public ResponseEntity<Page<UserModel>> getAllUsers(@PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC)
+	public ResponseEntity<Page<UserModel>> getAllUsers(SpecificationTemplate.UserSpec spec,
+														@PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC)
 																Pageable pageable) {
 		 
-		Page<UserModel> userModelPage = userService.findAll(pageable);
+		Page<UserModel> userModelPage = userService.findAll(spec, pageable);
 		return ResponseEntity.status(HttpStatus.OK).body(userModelPage);
 	}
 
