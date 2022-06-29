cfbc13d73915bc123463307be70430557a0baaa1
Igor Frey <igorfreyy97@gmail.com>
2022-06-29 13:08:46 -0300

    Adicionando e testando configs de log e seus níveis

--- authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -3,12 +3,15 @@ package com.ead.authuser.controllers;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,11 +24,14 @@ import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
 
+
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/auth")
 public class AuthenticationController {
 
+	Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
+	
 	@Autowired
 	UserService userService;
 
@@ -55,6 +61,18 @@ public class AuthenticationController {
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
 
+	}
+	
+	@GetMapping("/")
+	public String index() {
+		
+		logger.trace("TRACE");
+		logger.debug("DEBUG");
+		logger.info("INFO");
+		logger.warn("WARN");
+		logger.error("ERROR");
+		
+		return"Logging Spring Boot...";
 	}
 
 }
