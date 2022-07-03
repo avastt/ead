6c2111d1cbef9d101c70ebb0ac3b1001104e0eba
Igor Frey <igorfreyy97@gmail.com>
2022-07-03 20:28:25 -0300

    Relação bidirecional de User e Course criando uma tabela relacionamento

--- authuser/src/main/java/com/ead/authuser/controllers/UserController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -112,7 +112,7 @@ public class UserController {
 
 			userService.save(userModel);
 
-			log.debug("PUT updateUser userModel saved: {}", userModel.toString());
+			log.debug("PUT updateUser userId saved: {}", userModel.getUserId());
 			log.info("User updated successfully userId {}{}", userModel.getUserId());
 			
 			return ResponseEntity.status(HttpStatus.OK).body(userModel);
