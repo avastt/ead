045396df205bd60a4937707c67be354478a7f0e2
Igor Frey <igorfreyy97@gmail.com>
2022-07-19 17:23:21 -0300

    Criação de inserção de usuário em Curso

--- course/src/main/java/com/ead/course/clients/CourseClient.java
+++ course/src/main/java/com/ead/course/clients/CourseClient.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -29,14 +30,15 @@ public class CourseClient {
 	@Autowired
 	UtilsService utilService;
 	
-	String REQUEST_URI = "http://localhost:8087";
+	@Value("${ead.api.url.authuser}")
+	String REQUEST_URL_AUTHUSER;
 
 	public Page<UserDto> getAllUsersByCourse(UUID courseId, Pageable pageable) {
 		List<UserDto> searchResult = null;
 
         ResponseEntity<ResponsePageDto<UserDto>> result = null;
 
-		String url = utilService.createUrl(courseId, pageable);
+		String url = REQUEST_URL_AUTHUSER + utilService.createUrlGetAllUsersByCourse(courseId, pageable);
 
 		log.debug("Request URL: {}", url);
 		log.info("Request URL: {}", url);
