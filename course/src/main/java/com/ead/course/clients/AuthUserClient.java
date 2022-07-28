3ca726d474a2a2a7bebf17f5c90f6fcb0b448c86
Igor Frey <igorfreyy97@gmail.com>
2022-07-28 12:25:43 -0300

    Finalização da comunicação síncrona entre os ms

--- course/src/main/java/com/ead/course/clients/AuthUserClient.java
+++ course/src/main/java/com/ead/course/clients/AuthUserClient.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import com.ead.course.dtos.CourseUserDto;
 import com.ead.course.dtos.ResponsePageDto;
 import com.ead.course.dtos.UserDto;
 import com.ead.course.services.UtilsService;
@@ -59,5 +60,15 @@ public class AuthUserClient {
 	public ResponseEntity<UserDto> getOneUserById(UUID userId) {
 		String url = REQUEST_URL_AUTHUSER + "/users/" + userId;
 		return restTemplate.exchange(url, HttpMethod.GET, null, UserDto.class);
+	}
+
+	public void postSubscriptionUserInCourse(UUID courseId, UUID userId) {
+		String url = REQUEST_URL_AUTHUSER + "/users/" + userId + "/courses/subscription";
+		
+		var courseUserDto = new CourseUserDto();
+		courseUserDto.setCourseId(courseId);
+		courseUserDto.setUserId(userId);
+		
+		restTemplate.postForObject(url, courseUserDto, String.class);
 	}
 }
